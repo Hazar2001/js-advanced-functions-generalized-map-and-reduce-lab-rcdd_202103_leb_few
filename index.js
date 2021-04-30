ea6f59adb43b8d96f1a848a9ff2cd44cd91142dd
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-30 23:29:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(Array){
+
+  function map1(Array){
+      return Array.map(x => x * 2);
+  }
+
+  function map2(Array){
+      return Array.map(x => x * x);
+  }
+
+  function map3(Array){
+      return Array.map(x => x * x);
+  }
+
+
+return [map1(),map2(),map3()];
+}
